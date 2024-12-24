65362d018f51274fcda38e407fc011e9d88926a5
Jason Caldwell <jman8851@gmail.com>
2024-12-23 22:21:47 -0500

    wrote conditional code with alert. Removed 0 from invalid characters so all ages could be input and generate appropriate alerts depending on input age.

--- js/assignment.js
+++ js/assignment.js
@@ -5,12 +5,21 @@ const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
 
 // create a mutable variable called age and do not assign it a value.
-
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
     
     // Write your code below
+    if (isNaN(age)){
+        alert("Please enter a valid number");
+    } else if (age < 18){
+        alert("You cannot vote and you cannot purchase alcohol.");
+    } else if (age < 21) {
+        alert("You can vote, but you cannot purchase alcohol.");
+    } else if (age >= 21) {
+        alert("You can vote and purchase alchol.");
+    }
 
 }
 
@@ -21,7 +30,7 @@ submissionBtn.addEventListener("click", function () {
 
 
 function validateInput(event) {
-    const invalidCharacters = ["e", "E", "-", "0", "+"];
+    const invalidCharacters = ["e", "E", "-", "+"];
     if (invalidCharacters.includes(event.key)) {
         event.preventDefault();
     }
